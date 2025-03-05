8bdcf0ffa9dee6180dbef6b17212f9c7eef3789d
Henry Brooks <henbrooks6@gmail.com>
2025-03-05 12:16:50 +0000

    simple vaccine model and refactoring some code for convenience

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ Student id: 2422764
 """
 
 
-
+# Basic agent class for simple model
 class Agent:
     def __init__(self, state): 
         # State can be 'S' 'I' or 'R'
@@ -22,14 +22,11 @@ class Agent:
     def setState(self,state):
         self.state = state
 
-
-
 # Some initial values
-populationSize = 1000
+populationSize = 5000
 numSusceptible = populationSize - 1
 numInfected = 1
 numRecovering = 0
-
 #R0 = beta / gamma
 
 # Parameters - I think these were estimates for covid
@@ -38,80 +35,114 @@ recoveryRate = 1/14
 infectionRate = recoveryRate * Rnaught
 
 # Initialise a population of 1 infected and the rest susceptible.
+# Population is represented as a simple list of agents
 population = np.array([Agent("S") for i in range(populationSize)])
 population[populationSize-1].setState("I")
 population[populationSize-1].printState
 
-"""for i in range(len(population)):
-    population[i].printState()
-"""
-
-# Data for graphs
-infectedOverT = []
-susOverT = []
-recOverT = []
-
-count = 0
-# Main loop - add some stopping criteria - maybe number of infected stops changing?
-while numInfected != 0:
+population2 = np.array([Agent("S") for i in range(populationSize)])
+population2[populationSize-1].setState("I")
+population2[populationSize-1].printState
 
 
-    # Have inner loop of each individual
-    for i in range(populationSize):
-        agent = population[i]
-        r = np.random.choice([x for x in range(populationSize) if x != i])
-        agentb = population[r]
+def updateProcess(agent1, agent2, infectionRate, numInfected, numSusceptible):
+    # Infects with probability beta
+    if (agent1.state == "I" and agent2.state == "S"):
+        if random.random() < infectionRate:
+            agent2.setState("I")
+            numInfected += 1
+            numSusceptible -= 1
+    elif (agent1.state == "S" and agent2.state == "I"):
+        if random.random() < infectionRate:
+            agent1.setState("I")
+            numInfected += 1
+            numSusceptible -= 1
 
-        if (agent.state == "I" and agentb.state == "S"):
-            if random.random() < infectionRate:
-                agentb.setState("I")
-                numInfected += 1
-                numSusceptible -= 1
-        elif (agent.state == "S" and agentb.state == "I"):
-            if random.random() < infectionRate:
-                agent.setState("I")
-                numInfected += 1
-                numSusceptible -= 1
-
-    for i in range(populationSize):
-        recoverAgent = population[i]
-        if recoverAgent.state == "I":
-            if random.random() < recoveryRate:
-                recoverAgent.setState("R")
-                numRecovering += 1
-                numInfected -= 1
+    return numInfected, numSusceptible
 
 
-    if count % 10 == 0:
-        print("********")
-        print("Generation " , count)
-        print("Infected ", numInfected)
-        print("Susceptible ", numSusceptible)
-        print("Recovering ", numRecovering)
+# Would like to add more complex agents, where the infection rates are individual and based on vaccination / quarantining etc.
 
-    infectedOverT.append(numInfected)
-    susOverT.append(numSusceptible)
-    recOverT.append(numRecovering)
+def runSimulation(population, infectionRate, recoveryRate, vaccinate):
+    # Graph data
+    infectedOverT = []
+    susOverT = []
+    recOverT = []
 
-    count += 1
+    populationSize = 5000
+    numSusceptible = populationSize - 1
+    numInfected = 1
+    numRecovering = 0
+
+    if (vaccinate):
+        infectionRate *= 0.1
+
+    count = 0
+    # Main loop - Stopping criteria when no more infected individuals
+    while numInfected != 0 and count < 100:
 
 
-# Sample data: values over time
-time = np.arange(10)  # Time steps 0 to 9
-values = np.random.randint(1, 100, size=10)  # Random values
 
-# Plot the data
-plt.plot(range(len(infectedOverT)), infectedOverT, label="Infected")
+        # Have inner loop of each individual
+        if numSusceptible != 0: # for efficiency - all infected so no need to perform this loop
+        
+            for i in range(populationSize):
 
-plt.plot(range(len(infectedOverT)), susOverT, label="Susceptible")
+                # The selected individual meets a random individual (agentb) and then update process is applied.
+                agent = population[i]
+        
+                r = np.random.choice([x for x in range(populationSize) if x != i])
+                agentb = population[r]
 
-plt.plot(range(len(infectedOverT)), recOverT, label="Recovering")
+                # Apply the update process for two individuals
+                numInfected, numSusceptible = updateProcess(agent, agentb, infectionRate, numInfected, numSusceptible)
 
-# Labels and title
-plt.xlabel("Time")
-plt.ylabel("Number of individuals")
-plt.title("Susceptible, infected, and recovering over time")
-plt.legend()
+        # Check each agent recovers at this timestep
+        for i in range(populationSize):
+            recoverAgent = population[i]
+            if recoverAgent.state == "I":
+                if random.random() < recoveryRate:
+                    recoverAgent.setState("R")
+                    numRecovering += 1
+                    numInfected -= 1
 
-# Show the graph
+
+        if count % 1 == 0:
+            print("********")
+            print("Generation " , count)
+            print("Infected ", numInfected)
+            print("Susceptible ", numSusceptible)
+            print("Recovering ", numRecovering)
+
+        infectedOverT.append(numInfected)
+        susOverT.append(numSusceptible)
+        recOverT.append(numRecovering)
+
+        count += 1
+    
+    return infectedOverT, susOverT, recOverT
+
+infectedOverT, susOverT, recOverT = runSimulation(population, infectionRate, recoveryRate, vaccinate = False)
+
+# Simple vaccination assumption
+infectedOverTV, susOverTV, recOverTV = runSimulation(population2, infectionRate, recoveryRate, vaccinate = False)
+
+
+fig, axes = plt.subplots(1, 2, figsize=(10,4))
+
+axes[0].plot(range(len(infectedOverT)), infectedOverT, color='b', label='Infected over time')
+axes[0].plot(range(len(infectedOverT)), susOverT, color='b', label='Susceptible over time')
+axes[0].plot(range(len(infectedOverT)), recOverT, color='b', label='Recovered over time')
+axes[0].set_title("SIR results for unvaccinated")
+axes[0].legend()
+
+# Second plot (right)
+axes[1].plot(range(len(infectedOverTV)), infectedOverTV, color='b', label='Infected over time')
+axes[1].plot(range(len(infectedOverTV)), susOverTV, color='b', label='Susceptible over time')
+axes[1].plot(range(len(infectedOverTV)), recOverTV, color='b', label='Recovered over time')
+axes[1].set_title("SIR results for vaccinated")
+axes[1].legend()
+
+# Show the plots
+plt.tight_layout()
 plt.show()
